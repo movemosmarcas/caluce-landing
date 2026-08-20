9489d66fe3458786136014b13eaa5c8d7b891ea2
movemosmarcas <movemosmarcas@gmail.com>
2026-08-20 11:52:48 -0500

    Final working Google Sheets integration URL

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     params.append(pair[0], pair[1]);
                 }
                 
-                fetch('https://script.google.com/macros/s/AKfycbys3Zj8nvW1IlOysZb_o4v0lNvE2LONCYcVls8Wu1xglVx4bPwriVuRnxGP4RhRz_vEVA/exec', {
+                fetch('https://script.google.com/macros/s/AKfycbyLURkc8z_KQ6VQrvx0bXarpJi5FAUWD-cElD9gVxXQ-abSU8F6sWpbtOuExsmOUBULjg/exec', {
                     method: 'POST',
                     mode: 'no-cors',
                     headers: {
